82e9a7f9e1c6e118a98d26318bb3e6ba9ccb6679
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-04-14 12:44:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import GithubUser from './Components/GithubUser';
+import "./App.css"
 
-function App() {
+const App = () => {
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='app' >App default functional component</h1>
+      <div className='parent'>
+        <GithubUser username="samarth0606" />
+        <GithubUser username="sabeelhps" />
+        <GithubUser username="alok92iit" />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
